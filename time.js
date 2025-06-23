15261c6418123a1aece96635d0173aac49f504f5
Jacob-Parrott <jacobparrott57@gmail.com>
2025-06-23 10:20:53 -0500

    commenting

--- time.js
+++ time.js
@@ -1,4 +1,5 @@
 let time = document.querySelector('p.time');
+// Sets the current hour + minute to the time variable every 1/100th of a second(lol)
 setInterval(function() {
     let clocktime = new Date();
     time.innerHTML = `${(clocktime.getHours() < 10 ? '0' : '') + clocktime.getHours()}:${(clocktime.getMinutes() < 10 ? '0' : '') + clocktime.getMinutes()}`;
