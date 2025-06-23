c732a402ca8fb531ab1c0b3c8686307199b47f52
Jacob-Parrott <jacobparrott57@gmail.com>
2025-06-23 11:48:17 -0500

    windows can no longer escape bounds of the desktop / go into the taskbar

---
+++ mousePosDebug.js
@@ -0,0 +1,3 @@
+document.addEventListener('mousemove', function(event) {
+    console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
+});
--- windowControl.js
+++ windowControl.js
@@ -24,8 +24,24 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    if (elmnt.offsetTop - pos2 <= 353){
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    } else{
+        elmnt.style.top = "353px";
+    }
+    if (elmnt.offsetTop - pos2 >= 0){
+    } else{
+        elmnt.style.top = "0px";
+    }
+    if (elmnt.offsetLeft - pos1 <= 1200){
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    } else{
+        elmnt.style.left = "1200px";
+    }
+    if (elmnt.offsetLeft - pos1 >= 0){
+    } else{
+        elmnt.style.left = "0px";
+    }
   }
 
   function closeDragElement() {
