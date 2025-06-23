584c751d5c729777bc51d7b52946f03b9039890a
Jacob-Parrott <jacobparrott57@gmail.com>
2025-06-23 10:04:17 -0500

    Made minutes have 0 in front if less than 10

--- time.js
+++ time.js
@@ -1,5 +1,5 @@
 let time = document.querySelector('p.time');
 setInterval(function() {
     let clocktime = new Date();
-    time.innerHTML = `${clocktime.getHours()}:${clocktime.getMinutes()}`;
+    time.innerHTML = `${clocktime.getHours()}:${(clocktime.getMinutes() < 10 ? '0' : '') + clocktime.getMinutes()}`;
 }, 1);
