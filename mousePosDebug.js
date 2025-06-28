ad22baca33f23c53f58d12d300f87d4779bb7cac
Jacob-Parrott <jacobparrott57@gmail.com>
2025-06-27 19:05:06 -0500

    Finished About Me site, Started Project files. Deprecated Mouse debug.

--- mousePosDebug.js
+++
@@ -1,3 +0,0 @@
-// document.addEventListener('mousemove', function(event) 
-//    console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
-//});
---
+++ pages/projects/projects.js
@@ -0,0 +1,62 @@
+dragElement(document.getElementById("projects"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseout = closeDragElementOut;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    if (elmnt.offsetTop - pos2 >= 0) {
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    } else {
+      elmnt.style.top = "0px";
+    }
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+let closeButton = document.getElementById("projectsCloseButton");
+closeButton.addEventListener("click", function () {
+  projects.style.display = "none";
+});
+
+let projectsStartButton = document.getElementById("projectsStartButton");
+projectsStartButton.addEventListener("click", function () {
+  if (projects.style.display === "none" || projects.style.display === "") {
+    projects.style.display = "block";
+  } else {
+    projects.style.display = "none";
+  }
+});
+
+function closeDragElementOut(e) {
+  if (e.target != elmnt || elmnt.contains(e.target)) return;
+  closeDragElement();
+}
