5219beaca268d891f07b97d80fd653b2c31678cb
Jacob-Parrott <jacobparrott57@gmail.com>
2025-07-01 08:53:38 -0500

    added wallpaper page

---
+++ pages/wallpapers/wallpapers.js
@@ -0,0 +1,63 @@
+dragElement(document.getElementById("wallpapers"));
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseout = closeDragElementOut;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    if (elmnt.offsetTop - pos2 >= 0) {
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    } else {
+      elmnt.style.top = "0px";
+    }
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+let wallpapersCloseButton = document.getElementById("wallpapersCloseButton");
+wallpapersCloseButton.addEventListener("click", function () {
+  wallpapers.style.display = "none";
+});
+
+let wallpapersStartButton = document.getElementById("wallpapersStartButton");
+wallpapersStartButton.addEventListener("click", function () {
+  if (wallpapers.style.display === "none" || wallpapers.style.display === "") {
+    wallpapers.style.display = "block";
+  } else {
+    wallpapers.style.display = "none";
+  }
+});
+
+function closeDragElementOut(e) {
+  if (e.target != elmnt || elmnt.contains(e.target)) return;
+  closeDragElement();
+}
+
